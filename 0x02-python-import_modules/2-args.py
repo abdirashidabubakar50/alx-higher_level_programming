2249c452336af570b492cc8105f1f0eb8b808c63
Rashid <abdirashidabubakar7@gmail.com>
2024-06-13 11:35:54 -0700

    command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     argc = len(argv)
     if argc == 0:
-        print("0 arguments")
+        print("0 arguments.")
     else:
-        print("{} arguments{}:".format(argc, "" if argc == 1 else "s"))
+        print("{} argument{}:".format(argc, "" if argc == 1 else "s"))
         for i in range(argc):
             print("{}: {}".format(i + 1, argv[i]))
