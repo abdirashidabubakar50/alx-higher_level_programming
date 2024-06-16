9d2976066c476f555fda7e9cc6336e9322d3a5d3
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-06-16 15:36:18 +0300

    max_integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    max = my_list[0]
+    for i in range(len(my_list) - 1):
+        if max < my_list[i]:
+            max = my_list[i]
+    return max
