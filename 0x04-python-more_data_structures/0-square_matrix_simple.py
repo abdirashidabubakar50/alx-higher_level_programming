6400df79132b3ad80aa7dd08e5e0b71c75733be0
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-06-17 02:27:47 +0300

    matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     res = []
     for row in matrix:
         res_row = []
-        for i in range(len(matrix)):
+        for i in range(len(matrix) - 1):
             res_row.append(row[i] * row[i])
         res.append(res_row)
     return res
