9266f566990c5db8b54766a42edaade6a50a036b
Rashid <abdirashidabubakar7@gmail.com>
2024-06-18 16:21:20 -0700

    value my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 if (args[0] === undefined) {
-  console.log('No Argument');
+  console.log('No argument');
 } else {
   console.log(args[0]);
 }
