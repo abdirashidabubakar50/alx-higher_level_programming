d7bf850e4ee410014ec92f0c2b7615c886be71aa
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-06-18 13:20:35 +0300

    lambda now

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    res = []
-    for row in matrix:
-        res_row = []
-        length = len(row)
-        for i in range(length):
-            res_row.append((row[i] * row[i]))
-        res.append(res_row)
-    return res
+    return list(map(lambda row: list(map(lambda x: x * x, row)), matrix))
