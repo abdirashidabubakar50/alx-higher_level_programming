503c784c7419b3b8aa96eadb8a988223e21d9703
Rashid <abdirashidabubakar7@gmail.com>
2024-06-19 12:56:55 -0700

    second_biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,15 +4,7 @@ const arglength = args.length;
 if (arglength === 0 || args.length === 1) {
   console.log('0');
 } else {
-  let max = -Infinity;
-  let secondMax = -Infinity;
-  for (let i = 0; i < arglength; i++) {
-    if (args[i] > max) {
-      secondMax = max;
-      max = args[i];
-    } else if (args[i] > secondMax && args[i] < max) {
-      secondMax = args[i];
-    }
-  }
-  console.log(max);
+  const uniqueNumbers = [...new Set(args)];
+  uniqueNumbers.sort((a, b) => b - a);
+  console.log(uniqueNumbers[1]);
 }
