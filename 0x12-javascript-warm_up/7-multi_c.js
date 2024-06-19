0bb5cf09e97b223bf96021f7d55662f23828b9b2
Rashid <abdirashidabubakar7@gmail.com>
2024-06-18 17:37:28 -0700

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 const x = process.argv[2];
 const numX = Number(x);
 if (isNaN(numX)) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	for(let i = 0; i < numX; i++) {
-		console.log('C is fun')
-	}
+  for (let i = 0; i < numX; i++) {
+    console.log('C is fun');
+  }
 }
