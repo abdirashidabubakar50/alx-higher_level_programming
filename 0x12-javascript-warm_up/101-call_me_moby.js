962f787d7c23e42db328c55604572f4d18cf490d
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-06-20 13:02:59 +0300

    call me moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-function callMeMoby(x, theFunction) {
-    for (let i = 0; i < x; i++) {
-        theFunction();
-    }
+function callMeMoby (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 }
 module.exports.callMeMoby = callMeMoby;
