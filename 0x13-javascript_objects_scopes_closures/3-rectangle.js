cab3dcc8b4a5a858d35cf1cd644a4783dcd0c32e
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-06-20 23:08:16 +0300

    printing the rectangle as x

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      const Rectangle = {};
+      Rectangle.width = 0;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
