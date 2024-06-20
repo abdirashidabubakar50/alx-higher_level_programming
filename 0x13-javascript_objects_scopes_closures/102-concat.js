06753155698e7d84623c529856caff6734eda836
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-06-21 01:54:00 +0300

    concat

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const [fileA, fileB, fileC] = process.argv.slice(2);
+fs.readFile(fileA, 'utf8', (err, dataA) => {
+  if (err) throw err;
+  fs.readFile(fileB, 'utf8', (err, dataB) => {
+    if (err) throw err;
+    const dataC = dataA + '\n' + dataB + '\n';
+    fs.writeFile(fileC, dataC, (err) => {
+      if (err) throw err;
+    });
+  });
+});
