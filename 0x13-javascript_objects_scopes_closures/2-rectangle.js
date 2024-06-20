f7166f7f40df24befe76efaa2003e2f19fdaf739
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-06-20 22:48:03 +0300

    empty objects

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-        if (w <=0 || h <=0 || w === undefined || h === undefined){
-            const Rectangle = {};
-        }
-        else {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      const Rectangle = {};
+      Rectangle.width = 0;
+    } else {
+      this.width = w;
+      this.height = h;
     }
+  }
 }
 
 module.exports = Rectangle;
