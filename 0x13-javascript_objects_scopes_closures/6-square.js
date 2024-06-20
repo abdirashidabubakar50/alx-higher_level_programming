fb54beea4d6e5d8c86e86e63589f5e55d3c8d999
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-06-21 00:04:14 +0300

    Inheritance

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const basicSquare = require('./5-square');
+
+class Square extends basicSquare {
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      if (c === undefined) {
+        console.log('X'.repeat(this.width));
+      } else {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
