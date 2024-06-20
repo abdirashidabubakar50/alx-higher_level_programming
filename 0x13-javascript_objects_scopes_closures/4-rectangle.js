fcacae04ec4f07261141140803e38d368f1bc106
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-06-20 23:20:52 +0300

    rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -17,8 +17,9 @@ class Rectangle {
   }
 
   rotate () {
-    this.width = this.height;
-    this.height = this.width;
+    let temp = this.width 
+    this.width = this.height
+    this.height = temp
   }
 
   double () {
