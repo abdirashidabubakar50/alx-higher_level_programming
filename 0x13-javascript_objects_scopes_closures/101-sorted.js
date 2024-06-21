601e5ced87f0d36b14f491308e59582517db3d4d
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-06-21 20:19:21 +0300

    sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const { dict } = require('./101-data');
 const occurrencesDict = {};
-
 for (const userId in dict) {
   const occurrences = dict[userId];
   if (!occurrencesDict[occurrences]) {
@@ -9,4 +8,5 @@ for (const userId in dict) {
   }
   occurrencesDict[occurrences].push(userId);
 }
+
 console.log(occurrencesDict);
