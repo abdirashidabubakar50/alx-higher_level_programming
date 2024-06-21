f1fdf7ab09d81d4cb860558c8ed15bb66e0e614a
Rashid <abdirashidabubakar7@gmail.com>
2024-06-21 10:55:31 -0700

    let us see

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function () {
-  myObject.value++;
-};
+myObject.incr = function () {    
+    myObject.value++;
+  };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
