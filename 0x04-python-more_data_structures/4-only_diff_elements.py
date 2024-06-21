fc1738d17ca799e59bf6745e0d44cdc10176105d
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-06-22 00:12:22 +0300

    now let us see how different elements are printed

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    set_3 = set()
-    for element_1 in set_1:
-        for element_2 in set_2:
-            if (element_1 not in set_2):
-                set_3.add(element_1)
-            if (element_2 not in set_1):
-                set_3.add(element_2)
-            # elif (element_2 != element_1):
-            #     set_3.add(element_2)
-    return set_3
+    return set_1.symmetric_difference(set_2)
