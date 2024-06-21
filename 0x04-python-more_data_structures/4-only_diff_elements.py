1e7326b7630a577241b31896870fea0ef95e4214
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-06-21 23:51:58 +0300

    only different elements

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    set_3 = set()
+    for element_1 in set_1:
+        for element_2 in set_2:
+            if (element_1 not in set_2):
+                set_3.add(element_1)
+            elif (element_2 != element_1):
+                set_3.add(element_2)
+    return
