fad4baf7d2dcf484f4fe6417df89850e498faeab
Rashid <abdirashidabubakar7@gmail.com>
2024-06-26 15:01:43 -0700

    filter cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -29,13 +29,19 @@ if __name__ == "__main__":
             SELECT cities.name FROM cities
             JOIN states ON cities.state_id = states.id
             WHERE states.name = %s
+            ORDER BY cities.id ASC
 
             """
     cur.execute(query,(state_name,))
 
     rows = cur.fetchall()
+    unique_cities = list(set(rows))
+    
+    cities = []
+    for row in unique_cities:
+        cities.append(row[0])
 
-    for row in rows:
-        print(row)
+    print(", ".join(cities))
+
     cur.close()
     db.close()
