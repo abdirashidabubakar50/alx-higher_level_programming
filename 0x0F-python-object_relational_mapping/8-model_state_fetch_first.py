54eafa28aa6fb890b0f57311b547a9be2d75e5be
Rashid <abdirashidabubakar7@gmail.com>
2024-06-27 14:29:57 -0700

    nightmares with checkers

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
 """
-This is a script that prints
+
+This is a script that prints the first State object from the database
 
 """
 from sys import argv
@@ -10,20 +10,18 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    db_name = argv[3]
-    # create an engine and connect to the MySQL database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        username, password, db_name
-        ), pool_pre_ping=True)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
-
     session = Session()
     state = session.query(State).order_by(State.id).first()
-    if state:
+
+    if state is not None:
         print("{}: {}".format(state.id, state.name))
     else:
-        print("Nothing")
+        print("nothing")
+
+    session.close()
