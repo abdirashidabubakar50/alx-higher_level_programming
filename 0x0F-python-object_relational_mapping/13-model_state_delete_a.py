58cf686d772f70a8015b5e9f0657673eb6fb0467
Rashid <abdirashidabubakar7@gmail.com>
2024-06-28 13:03:12 -0700

    deleting from the database using ORM

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     session = Session()
 
     state_a = session.query(State).filter(
-            State.name.like('%a%')).order_by(State.id).all()
+            State.name.like('%a%')).all()
 
     for state in state_a:
         session.delete(state)
