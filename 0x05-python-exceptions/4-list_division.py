f9f9c1f5da955cafdca1092bf2cda4c29f586e5f
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-06-29 16:53:16 +0300

    Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
             # divide the elements in the two lists
             result = my_list_1[i] / my_list_2[i]
         except TypeError:
-            print("wrong Type")
+            print("wrong type")
             result = 0
         except ZeroDivisionError:
             print("division by 0")
