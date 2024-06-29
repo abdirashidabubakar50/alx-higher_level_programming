675f64797f3d971fbedb80fec2874c241aad77b2
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-06-29 23:50:52 +0300

    my first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,27 +1,11 @@
 #!/usr/bin/python3
 """
-This is module defines a class 'Square' which represents a square
-with a given height and width
+This is module defines an empty class 'Square'
 
 """
 
 class Square:
     """
-    This is a class that defines a square by its height
-    and width 
-
-    Attributes:
-        height (int): The height of the square. Defaults to 0
-        widht (int): The width of the square. Defaults to 0
+    This is an empty class that defines square
     """
-
-    def __init__(self, height=0, width=0):
-        """
-        Initializes a new instance of the Square class.
-
-        Args:
-            height (int): The height of the square. Defautls to 0
-            width (int): The width of the square. Defaults to 0
-        """
-        self.height = height
-        self.width = width
+    pass
