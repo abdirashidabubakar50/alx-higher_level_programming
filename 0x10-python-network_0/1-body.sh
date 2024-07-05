44f4345de4e594193fd60c7de64b3e2e970c5751
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-05 09:06:12 -0700

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This script takes in a url, sends a GET request to the URL, and displays the body of the response
-curl -s -o /dev/null -w "%http_code%" "$1" | grep -q 200 && curl -s "$1"
+curl -sL -w "%{http_code}" "$1" -o response_body | { read status; [ "$status" -eq 200 ] && cat response_body; }
