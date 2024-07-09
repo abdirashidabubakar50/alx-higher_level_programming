8097d554907d0749e3d1cc83244545b03a273d40
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-09 17:41:32 +0300

    displaying headers/

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,7 +9,7 @@ header of the response
 import urllib.request
 import sys
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
