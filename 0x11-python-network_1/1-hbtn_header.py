324f579e0b8da920166ff0ff36cbad6f68a05f44
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-09 08:34:59 -0700

    Response headers

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
-        headers = response.info()
-        x_request_id = headers.get('X-Request-Id')
-        if x_request_id:
-            print(x_request_id)
+        headers = response.getheader('X-Request-Id')
+        if headers:
+            print(headers)
