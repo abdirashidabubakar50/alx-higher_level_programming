97601a288919000d2e74fed65388abbd92008ae4
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-18 00:58:38 +0300

    Read File

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -13,6 +13,6 @@ def read_file(filename=""):
         filename(str): The name of the file to read. Defaults to
                         an empty string.
     """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         content = f.read()
-    print(content)
+    print(content, end="")
