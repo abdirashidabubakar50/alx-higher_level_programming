9f08bc8ced358ea6d7a089f4988e6e65b02b4ed0
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-19 22:51:29 +0300

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,9 +5,24 @@ This module defines a class Base
 
 
 class Base:
+    """
+    Base class for  managing id attribute in all future classes.
+
+    Attributes:
+        __nb_objects(int): Private class attribute that keeps track of the
+        the number of instances created without a specified id.
+
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Initializes a new Base instance
+
+        Args:
+            id(int, optional): The id for the instance. If None,
+            an id is automatically assigned
+        """
         if id is not None:
             self.id = id
         else:
