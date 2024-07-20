cf280c5885214e5a0186ff1bca7ab3e004fba94d
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-20 18:03:15 +0300

    running tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,8 +2,9 @@
 import unittest
 from models.base import Base
 
+
 class TestBase(unittest.TestCase):
-    
+
     def test_auto_assign_id(self):
         """Test Automatic id assignment"""
         instance1 = Base()
@@ -12,6 +13,7 @@ class TestBase(unittest.TestCase):
         self.assertEqual(instance1.id, 1)
         self.assertEqual(instance2.id, 2)
         self.assertEqual(instance3.id, 3)
+
 
 if __name__ == '__main__':
     unittest.main()
