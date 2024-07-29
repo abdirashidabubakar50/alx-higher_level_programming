860464e86d8a112b84103323044bbdd7252d06f7
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-30 02:57:31 +0300

    dictionary representation of the square instance

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -59,3 +59,12 @@ class Square(Rectangle):
             for key, value in kwargs.items():
                 if hasattr(self,  key):
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
