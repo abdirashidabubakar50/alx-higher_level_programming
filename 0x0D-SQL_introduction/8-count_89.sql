95f4f045c2b58972a012d281370a9daec6e1ef58
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-30 15:15:02 +0300

    count 89

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- A script that displays the number of records with id = 89 in the table first_table
+
+-- displaying the number of records with id = 89
+SELECT COUNT(*) FROM first_table WHERE id = 89;
