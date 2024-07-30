47ce1caf52f3f0d735c1a689b89a9d7f3a0b4c1d
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-30 16:57:31 +0300

    static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,6 +30,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         Converts a list of dictionaries to a JSON string
