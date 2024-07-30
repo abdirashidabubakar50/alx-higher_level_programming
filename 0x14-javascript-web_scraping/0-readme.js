8293599e6dca917b1d835dfb477bbf906cf24588
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-30 22:48:27 +0300

    write me

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,24 +1,2 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-
-// Get the file path from the command line arguments
-const filePath = process.argv[2];
-
-// Read the file content in utf-8
-fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        // Print the detailed error object if an error occurred
-        console.log({
-            Error: `ENOENT: no such file or directory, open '${filePath}'`,
-            at: 'Error (native)',
-            errno: err.errno,
-            code: err.code,
-            syscall: err.syscall,
-            path: filePath
-        });
-        return;
-    }
-    // Print the file content
-    console.log(data);
-});
