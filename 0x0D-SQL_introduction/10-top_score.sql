6a3faae228dd7087b0ccd26208410bd14529abda
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-30 16:26:51 +0300

    listing all records of the table in descending order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- A script  that lists all records of the table second_table of the database hbtn_0c_0
+
+
+-- listing all records of the table second_table in descending order
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
