9171b683aaf1d493656dc06d1f9cebf425ddf8e8
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-30 15:05:17 +0300

    printing full description of a table in a dataabase

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- A script that prints the full description of the table first_table
+-- from the database hbtn_0c_0 
+
+-- prints the full description of the table first_table
+SHOW CREATE TABLE first_table;
