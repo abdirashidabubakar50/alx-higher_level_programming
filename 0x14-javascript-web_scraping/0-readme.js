c49a4055bace41001e82788325ba4ba93763252e
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-30 22:38:02 +0300

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,17 +2,23 @@
 
 const fs = require('fs');
 
-if (process.argv.length < 3) {
-  console.error('Usage: ./0-readme.js <file_path>');
-  process.exit(1);
-}
-
+// Get the file path from the command line arguments
 const filePath = process.argv[2];
 
+// Read the file content in utf-8
 fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(data);
+    if (err) {
+        // Print the detailed error object if an error occurred
+        console.log({
+            Error: `ENOENT: no such file or directory, open '${filePath}'`,
+            at: 'Error (native)',
+            errno: err.errno,
+            code: err.code,
+            syscall: err.syscall,
+            path: filePath
+        });
+        return;
+    }
+    // Print the file content
+    console.log(data);
 });
