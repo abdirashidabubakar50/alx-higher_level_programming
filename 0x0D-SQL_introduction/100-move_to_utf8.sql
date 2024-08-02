683915378108c858e95ded320e8e4751ab4fd70a
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-08-02 22:44:46 +0300

    converting to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,20 @@
+--  script that converts hbtn_0c_0 database to UTF8
+
+-- converting  database to UTF8
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+
+USE hbtn_0c_0
+
+-- convert the table character set and collation
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- convert the column character set and collation
+ALTER TABLE first_table
+MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
